27426e810bd8ffd78925354f5f383fd7785ab2ee
KhodeCipher <lavezziomotola@gmail.com>
2022-09-14 19:54:45 +0100

    commit

---
+++ welcome.php
@@ -0,0 +1,7 @@
+<?php
+if (!isset($_POST['submit'])){
+    echo "error, you need to submit the form!";
+}
+$name = $_POST['exampleInputName1']
+$email=$_POST['exampleInputEmail1']
+$phonenumber=$_POST
